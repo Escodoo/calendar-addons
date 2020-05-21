479b18a3d57b239f4041de3dc58ac92d2e14a493
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-21 16:54:03 -0300

    [12.0][ADD] New module Calendar Meeting Minutes

---
+++ calendar_meeting_minutes/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Calendar Meeting Minutes',
+    'summary': """
+        This module enable report meeting minutes.""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'www.escodoo.com.br',
+    'depends': [
+        'calendar',
+    ],
+    'data': [
+            'views/calendar_event_view.xml',
+            'report/calendar_event_meeting_minutes_report.xml',
+    ],
+    'demo': [
+    ],
+}
